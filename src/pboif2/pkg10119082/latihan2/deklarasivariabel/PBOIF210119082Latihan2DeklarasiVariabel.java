112e98822c2561a5cfb72af0d320d6558bdc5cbf
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-10-17 16:35:36 +0700

    PBOIF2-10119082-Latihan2-DeklarasiVariabel

---
+++ src/pboif2/pkg10119082/latihan2/deklarasivariabel/PBOIF210119082Latihan2DeklarasiVariabel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi Program : Deklarasi & Pengaksesan
+ */
+public class PBOIF210119082Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai=" + nilaiInt);
+        System.out.println("Isi variabel PHI=" + PHI);
+        System.out.println("Isi variabel logika=" + nilaiLogika);
+        System.out.println("Isi variabel karakter=" + nilaiKarakter);
+    }
+    
+}
